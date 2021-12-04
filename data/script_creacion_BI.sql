41642ef6bf2ed6e3cb09ef0585fb99ba23f460df
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-12-04 01:13:58 -0300

    fix: tables and views

--- data/script_creacion_BI.sql
+++ data/script_creacion_BI.sql
@@ -324,50 +324,6 @@ CREATE TABLE [SIN_NOMBRE].BI_MATERIAL
  )
 
 /**
- * ---------------------------------------------------------------------------------------------
- * CAMION VIAJE
- * ---------------------------------------------------------------------------------------------
- */
-
-
- CREATE TABLE [SIN_NOMBRE].[BI_CAMION_VIAJE] (
-	Id_Camion_Viaje INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	Patente   NVARCHAR(15) NOT NULL,
-	Recorrido INT,
-	Legajo_Chofer INT,
-	Fecha_Inicio DATETIME2(3),
-	Fecha_Fin DATETIME2(3),
-	Consumo_Combustible DECIMAL(18, 0),
-	Precio_Final DECIMAL(18, 0)
-)
-
-/* ---------------------------------------------------------------------------------------------
- * MANTENIMIENTO CAMION
- * ---------------------------------------------------------------------------------------------
- */
-
-CREATE TABLE [SIN_NOMBRE].BI_CAMION_MANTENIMIENTO(
-    Id_Camion_Mantenimiento INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-    Nro_OT INT, 
-	Patente_Camion NVARCHAR(15),
-	Modelo_Id SMALLINT,
-	Marca_Id SMALLINT,
-	Legajo INT,
-	Id_taller INT,
-	Fecha_Creacion_OT DATETIME2(3), -- De la OT
-	Fecha_Fin_OT DATETIME2(3), -- Calculado
-	Cod_Tarea INT,
-	Fecha_Inicio_Planificado DATETIME2(3), -- De la tarea
-	Fecha_Inicio_Real DATETIME2(3),      ---/
-	Fecha_Fin_Real DATETIME2(3),         --/
-	Cantidad_Horas_Trabajadas INT,
-	Material NVARCHAR(100),
-	Material_Cantidad INT,
-)
-GO
-
-
-/**
  * =============================================================================================
  * ALTERS
  * =============================================================================================
@@ -402,21 +358,6 @@ ALTER TABLE [SIN_NOMBRE].[BI_HECHO_ORDEN_TRABAJO] WITH CHECK ADD
 	,CONSTRAINT [FK_bi_h_ot_taller]		FOREIGN KEY(Id_Taller) REFERENCES [SIN_NOMBRE].[BI_TALLER]
 	,CONSTRAINT [FK_bi_h_ot_tarea]		FOREIGN KEY(Tarea_Mas_Utilizada) REFERENCES [SIN_NOMBRE].[BI_TAREA]
 	,CONSTRAINT [FK_bi_h_ot_material]	FOREIGN KEY(Material_Mas_Utilizado) REFERENCES [SIN_NOMBRE].[BI_MATERIAL]
- GO
-
-ALTER TABLE [SIN_NOMBRE].[BI_CAMION_VIAJE] WITH CHECK ADD
-	CONSTRAINT [FK_bi_camion_viaje_recorrido] FOREIGN KEY(Recorrido) REFERENCES [SIN_NOMBRE].[BI_RECORRIDO],
-	CONSTRAINT [FK_bi_camion_viaje_chofer]	FOREIGN KEY(Legajo_Chofer) REFERENCES [SIN_NOMBRE].[BI_CHOFER],
-	CONSTRAINT [FK_bi_camion_viaje]			FOREIGN KEY(Patente) REFERENCES [SIN_NOMBRE].[BI_CAMION]
-GO
-
- ALTER TABLE [SIN_NOMBRE].[BI_CAMION_MANTENIMIENTO] WITH CHECK ADD
-	CONSTRAINT [FK_bi_mantenimiento_material] FOREIGN KEY(Material) REFERENCES [SIN_NOMBRE].[BI_MATERIAL]
-	,CONSTRAINT [FK_bi_mantenimiento_mecanico] FOREIGN KEY(Legajo) REFERENCES [SIN_NOMBRE].[BI_MECANICO]
-	,CONSTRAINT [FK_bi_mantenimiento_modelo] FOREIGN KEY(Marca_Id, Modelo_Id) REFERENCES [SIN_NOMBRE].[BI_MODELO_CAMION] (Marca_Id, Modelo_Id)
-	,CONSTRAINT [FK_bi_mantenimiento_talller] FOREIGN KEY(Id_Taller) REFERENCES [SIN_NOMBRE].[BI_TALLER]
-	,CONSTRAINT [FK_bi_mantenimiento_tarea] FOREIGN KEY(Cod_Tarea) REFERENCES [SIN_NOMBRE].[BI_TAREA]
-	,CONSTRAINT [FK_bi_mantenimiento_camion] FOREIGN KEY(Patente_Camion) REFERENCES [SIN_NOMBRE].[BI_CAMION]
  GO
 
 /**
@@ -708,237 +649,134 @@ GO
 EXEC [SIN_NOMBRE].PR_INSERT_HECHO_OT
 GO
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-INSERT INTO [SIN_NOMBRE].[BI_CAMION_VIAJE]
-SELECT CAST(V.Patente_Camion as NVARCHAR(15))
-	, V.Cod_Recorrido
-	, V.Legajo_Chofer
-	, V.Fecha_Inicio
-	, V.Fecha_Fin
-	, V.Consumo_Combustible
-	, V.Precio_Final + (SELECT SUM(PxV.Cantidad * TP.Precio) AS 'VALOR_TOTAL_PAQUETES'
-	FROM [SIN_NOMBRE].PAQUETE_POR_VIAJE PxV
-	JOIN [SIN_NOMBRE].PAQUETE P ON PxV.Id_Paquete = P.Id
-	JOIN [SIN_NOMBRE].TIPO_PAQUETE TP ON P.Tipo = TP.Codigo
-	WHERE PxV.Id_Viaje = V.Id) AS 'PRECIO_TOTAL_VIAJE'
-FROM [SIN_NOMBRE].VIAJE V
-GO
-
-INSERT INTO [SIN_NOMBRE].[BI_CAMION_MANTENIMIENTO]
-SELECT 
-	OT.Nro_OT
-	,CAST(OT.Patente_Camion as NVARCHAR(15))
-	,C.Modelo_Id
-	,C.Marca_Id
-	,TxO.Mecanico
-	,M.Id_taller
-	,OT.Fecha_Creacion
-	, (select MAX(tpo.Fecha_Fin_Real) from [SIN_NOMBRE].TAREA_POR_ORDEN tpo where tpo.Nro_OT = OT.Nro_OT) -- Fecha fin OT
-	,TxO.Cod_Tarea
-	,TxO.Fecha_Inicio_Planificado
-	,TxO.Fecha_Inicio_Real
-	,TxO.Fecha_Fin_Real
-	,(DATEDIFF (DAY, TxO.Fecha_Inicio_Real , TxO.Fecha_Fin_Real) * 8) AS [Cantidad_Horas_Trabajadas]
-	,MxT.Cod_Material
-	,ISNULL(MxT.Cantidad, 1) AS [Cantidad]
-FROM [SIN_NOMBRE].ORDEN_TRABAJO OT
-JOIN [SIN_NOMBRE].CAMION C				ON C.Patente = OT.Patente_Camion
-JOIN [SIN_NOMBRE].TAREA_POR_ORDEN TxO	ON TxO.Nro_OT = OT.Nro_OT
-JOIN [SIN_NOMBRE].MECANICO M			ON M.Legajo = TxO.Mecanico
-JOIN [SIN_NOMBRE].MATERIAL_POR_TAREA MxT ON MxT.Cod_Tarea = TxO.Cod_Tarea
-GO
-
-/**
+/*
  * =============================================================================================
  * VIEWS
  * =============================================================================================
  */
 
-
---------------------5 TAREAS MÁS REALIZADAS POR MODELO CAMIÓN------------------
-
-/*
-SELECT cm.modelo_camion, t.descripcion
-FROM [SIN_NOMBRE].[BI_CAMION_MANTENIMIENTO] cm
-INNER JOIN [SIN_NOMBRE].[BI_TAREA] T ON T.Codigo = CM.Cod_Tarea
-WHERE cm.cod_tarea in (select top 5 cod_tarea 
-						from [SIN_NOMBRE].[BI_CAMION_MANTENIMIENTO] 
-						group by cod_tarea
-						order by count(cod_tarea))
-GROUP BY cm.modelo_camion
-*/
-
-
- --select cm.Modelo_Id, cm.Cod_Tarea, count(distinct cm.Cod_Tarea)
- --from SIN_NOMBRE.BI_CAMION_MANTENIMIENTO cm
- --group by cm.Modelo_Id, cm.Cod_Tarea
- --order by cm.Modelo_Id, cm.Cod_Tarea
-
- CREATE VIEW [SIN_NOMBRE].[V_Camion_Costo_Mant] AS
- Select cm.patente_camion, cm.id_taller
-	, DATEPART(quarter, cm.Fecha_Fin_Real) as 'Cuatrimestre'
-	, sum( (m.precio * cm.Material_Cantidad) + (cm.cantidad_horas_trabajadas * me.costo_hora)) as costo_total
- from SIN_NOMBRE.BI_CAMION_MANTENIMIENTO cm 
- inner join SIN_NOMBRE.BI_MECANICO me on cm.Legajo = me.legajo
- inner join SIN_NOMBRE.BI_MATERIAL m on cm.material = m.codigo
- group by cm.patente_camion, cm.id_taller, DATEPART(quarter, cm.Fecha_Fin_Real)
- GO
-
-CREATE VIEW [SIN_NOMBRE].[V_Camion_Maximo_Tiempo_FueraDeServicio] AS
-select cm.Patente_Camion
-	, DATEPART(quarter, cm.Fecha_Creacion_OT) as 'Cuatrimestre'
-	, max(DATEDIFF(DAY, cm.Fecha_Creacion_OT, cm.Fecha_Fin_OT)) as 'Maxima_Duracion_OT (Dias)'
-from SIN_NOMBRE.BI_CAMION_MANTENIMIENTO cm
-group by cm.Patente_Camion, DATEPART(quarter, cm.Fecha_Creacion_OT);
+-- Maximo Tiempo Fuera de Servicio
+CREATE OR ALTER VIEW [SIN_NOMBRE].[V_Camion_Maximo_Tiempo_FueraDeServicio] AS
+SELECT
+	T.Anio
+	,T.Cuatrimestre
+	,OT.Patente		AS [Camion]
+	,MAX(OT.Horas_Sin_Servicio) AS [Sin_Servicio]
+FROM [SIN_NOMBRE].[BI_HECHO_ORDEN_TRABAJO] OT
+JOIN [SIN_NOMBRE].[BI_TIEMPO] T ON T.Id = OT.Tiempo
+GROUP BY T.Anio, T.Cuatrimestre, OT.Patente
 GO
 
-CREATE VIEW [SIN_NOMBRE].[V_Facturacion_Recorrido_Cuatrimestre] AS
-SELECT r.Codigo as 'Recorrido'
-	, r.Origen as 'Ciudad_Origen'
-	, r.Destino as 'Ciudad_Destino'
-	, DATEPART(QUARTER, Fecha_Inicio) as 'Cuatrimestre'
-	, SUM(Precio_Final) as 'Facturacion_Total'
-FROM SIN_NOMBRE.BI_CAMION_VIAJE cv
-JOIN SIN_NOMBRE.BI_RECORRIDO r on cv.Recorrido = r.Codigo
-GROUP BY r.Codigo, r.Origen, r.Destino, DATEPART(QUARTER, Fecha_Inicio);
---ORDER BY r.Codigo, DATEPART(QUARTER, Fecha_Inicio);
-GO
-
-CREATE VIEW [SIN_NOMBRE].[V_Camion_Ganancias] AS
-select cm.Patente_Camion
-	, SUM(cv.Precio_Final) as 'Ingresos'
-	, SUM(ch.Costo_Hora * (DATEDIFF(HOUR, cv.Fecha_Inicio, cv.Fecha_Fin)) + 100 * cv.Consumo_Combustible) as 'Costo_Viaje'
-	, SUM(cm.Material_Cantidad * mt.Precio + cm.Cantidad_Horas_Trabajadas * mec.Costo_Hora) as 'Costo_Mantenimiento'
-	, SUM(cv.Precio_Final)
-		-
-	SUM(ch.Costo_Hora * (DATEDIFF(HOUR, cv.Fecha_Inicio, cv.Fecha_Fin)) + 100 * cv.Consumo_Combustible)
-		-
-	SUM(cm.Material_Cantidad * mt.Precio + cm.Cantidad_Horas_Trabajadas * mec.Costo_Hora)
-	as 'Ganancia'
-from SIN_NOMBRE.BI_CAMION_VIAJE cv
-join SIN_NOMBRE.BI_CAMION_MANTENIMIENTO cm on cv.Patente = cm.Patente_Camion
-join SIN_NOMBRE.BI_CHOFER ch on cv.Legajo_Chofer = ch.Legajo
-join SIN_NOMBRE.BI_MATERIAL mt on cm.Material = mt.Codigo
-join SIN_NOMBRE.BI_MECANICO mec on cm.Legajo = mec.Legajo
-group by cm.Patente_Camion;
-GO
-
-CREATE VIEW [SIN_NOMBRE].[V_Promedio_Costo_Chofer]
-AS SELECT 
-	CASE WHEN Edad BETWEEN 18 AND 30 THEN '18 - 30'
-			  WHEN Edad BETWEEN 31 AND 50 THEN '31 - 50'
-			  ELSE '> 50' END AS [Rango_Etario]
-	,AVG(Costo_Hora) AS [Promedio]
-FROM [SIN_NOMBRE].[BI_CHOFER]
-GROUP BY CASE WHEN Edad BETWEEN 18 AND 30 THEN '18 - 30'
-			  WHEN Edad BETWEEN 31 AND 50 THEN '31 - 50'
-			  ELSE '> 50' END
-GO
-
-
-CREATE VIEW [SIN_NOMBRE].[V_Desvio_Tareas]
-AS
-SELECT DISTINCT
-      [Id_taller]	AS [Taller]
-      ,[Cod_Tarea]	AS [Tarea]
-      ,AVG ( T.Tiempo_Estimado  - [Cantidad_Horas_Trabajadas]/8) AS [Desvio]
-  FROM (SELECT DISTINCT
-		CM2.Nro_Ot,
-		CM2.Cod_Tarea,
-		CM2.Id_taller,
-		CM2.Cantidad_Horas_Trabajadas
-		FROM [SIN_NOMBRE].[BI_CAMION_MANTENIMIENTO] CM2) CM
-JOIN [SIN_NOMBRE].[BI_TAREA] T ON T.Codigo = CM.Cod_Tarea
-GROUP BY CM.Id_taller, CM.Cod_Tarea
-GO
-
-
--- Para el TOP 5 Tareas por Camion
-
--- Primero Vistamos las Tareas Realizadas
-CREATE VIEW [SIN_NOMBRE].[V_Tareas_Realizadas]
-AS
-SELECT DISTINCT       
-		[Nro_OT]
-	  ,[Cod_Tarea]
-	  ,[Marca_Id]
-      ,[Modelo_Id]
-	  ,[Id_Taller]
-  FROM [SIN_NOMBRE].[BI_CAMION_MANTENIMIENTO]
-GO
--- Despues Las Tareas Por Camion
-CREATE VIEW [SIN_NOMBRE].[V_Tareas_Por_Camion]
-AS
-SELECT [Cod_Tarea]
-	  ,[Marca_Id]
-	  ,[Modelo_Id]
-      ,ISNULL(COUNT(*), 0) AS [Ejecutadas]
-FROM [SIN_NOMBRE].[V_Tareas_Realizadas]
-GROUP BY [Cod_Tarea], [Marca_Id], [Modelo_Id]
-GO
-
--- Finalmente Los Camiones con Sus respectivas Tareas
-CREATE VIEW [SIN_NOMBRE].[V_TOP_5_Tareas_Por_Camion]
-AS
-SELECT DISTINCT
-   T.[Marca_ID]
-  ,T.[Modelo_ID]
-  ,T.[Cod_Tarea]
-  ,TE.Ejecutadas AS [Veces Realizas]
-FROM [SIN_NOMBRE].[V_Tareas_Realizadas] T
-JOIN [SIN_NOMBRE].[V_Tareas_Por_Camion] TE ON 
-	TE.Modelo_Id = T.Modelo_Id AND TE.Cod_Tarea = T.Cod_Tarea
-	AND TE.Marca_Id = T.Marca_Id
-WHERE T.Cod_Tarea IN (SELECT TOP 5 Cod_Tarea
-						FROM [SIN_NOMBRE].[V_Tareas_Por_Camion] TxC
-						WHERE T.Marca_Id = TxC.Marca_Id
-						AND T.Modelo_Id = TxC.Modelo_Id
-						ORDER BY Ejecutadas DESC)
-GO
-
-CREATE VIEW [SIN_NOMBRE].[V_Materiales_Taller]
+-- Costo Mantenimiento Por Cuatrimestre
+CREATE OR ALTER VIEW [SIN_NOMBRE].[V_Camion_Costo_Mant] 
 AS
 SELECT
-       [Id_taller]
-      ,[Material]
-      ,SUM([Material_Cantidad]) AS [Veces_Utilizado]
-  FROM [GD2C2021].[SIN_NOMBRE].[BI_CAMION_MANTENIMIENTO]
-GROUP BY Id_taller, Material
+	T.Anio
+	,T.Cuatrimestre
+	,OT.Patente
+	,SUM(OT.Costo_Total) AS [Costo Total Cuatrimestral]
+FROM [SIN_NOMBRE].[BI_HECHO_ORDEN_TRABAJO] OT
+JOIN [SIN_NOMBRE].[BI_TIEMPO] T ON T.Id = OT.Tiempo
+GROUP BY T.Anio, T.Cuatrimestre, OT.Patente
 GO
 
-CREATE VIEW [SIN_NOMBRE].[V_Top_10_Materiales_Taller]
+
+-- DESVIO PROMEDIO POR TALLER
+CREATE OR ALTER VIEW [SIN_NOMBRE].[V_Desvio_Tareas]
 AS
 SELECT DISTINCT
-	 CM.[Id_taller] AS [Taller]
-	,CM.[Material]
-	,VMT.Veces_Utilizado
-FROM [SIN_NOMBRE].[BI_CAMION_MANTENIMIENTO] CM
-JOIN [SIN_NOMBRE].[V_Materiales_Taller]  VMT ON VMT.Id_taller = CM.Id_taller AND VMT.Material = CM.Material
-WHERE CM.Material IN (SELECT TOP 10 
-							Material
-						FROM [SIN_NOMBRE].[V_Materiales_Taller] MT
-						WHERE MT.Id_taller = CM.Id_taller
-						ORDER BY MT.[Veces_Utilizado] DESC)
+      OT.[Id_taller]				AS [Taller]
+	  ,OT.[Tarea_Mas_Utilizada]		AS [Tarea]
+      ,AVG(Desvio_Tarea)			AS [Promedio Desvio]
+  FROM [SIN_NOMBRE].BI_HECHO_ORDEN_TRABAJO OT
+  GROUP BY OT.Id_taller, OT.Tarea_Mas_Utilizada
+GO
+
+-- TOP 5 Tareas Mas Utilizadas Por Camion
+CREATE OR ALTER VIEW [SIN_NOMBRE].[V_TOP_5_Tareas_Por_Camion]
+AS
+SELECT DISTINCT 
+	OT.Marca
+	,OT.Modelo
+	,OT.Tarea_Mas_Utilizada
+FROM [SIN_NOMBRE].BI_HECHO_ORDEN_TRABAJO OT
+WHERE OT.Tarea_Mas_Utilizada IN (
+									SELECT TOP 5
+									OT2.Tarea_Mas_Utilizada
+									FROM [SIN_NOMBRE].BI_HECHO_ORDEN_TRABAJO OT2
+									WHERE OT2.Marca = OT.Marca AND OT2.Modelo = OT.Modelo
+									GROUP BY OT2.Tarea_Mas_Utilizada
+									ORDER BY COUNT(OT2.Tarea_Mas_UTILIZADA) DESC
+								)
+GO
+
+-- TOP 10 Materiales mas utilizados por Taller
+CREATE OR ALTER VIEW [SIN_NOMBRE].[V_Top_10_Materiales_Taller]
+AS
+SELECT DISTINCT
+	OT.Id_taller AS [Taller]
+	,OT.Material_Mas_Utilizado AS [Material]
+FROM [SIN_NOMBRE].BI_HECHO_ORDEN_TRABAJO OT
+WHERE OT.Material_Mas_Utilizado IN (
+									SELECT TOP 10
+									 OT2.Material_Mas_Utilizado
+									FROM [SIN_NOMBRE].BI_HECHO_ORDEN_TRABAJO OT2
+									WHERE OT2.Id_taller = OT.Id_taller
+									GROUP BY OT2.Material_Mas_Utilizado
+									ORDER BY COUNT(OT2.Material_Mas_Utilizado) DESC
+									)
+GO
+
+--- Facturacion Total Por Recorrido
+CREATE OR ALTER VIEW [SIN_NOMBRE].[V_Facturacion_Recorrido_Cuatrimestre] AS
+SELECT 
+	T.Anio
+	,T.Cuatrimestre
+	,V.Recorrido
+	,R.Origen	AS [Ciudad_Origen]
+	,R.Destino	AS [Ciudad_Destino]
+	,SUM(V.Total_Facturado) AS [Facturacion_Cuatrimestral]
+FROM [SIN_NOMBRE].BI_HECHO_VIAJE V
+JOIN SIN_NOMBRE.BI_RECORRIDO R on V.Recorrido = R.Codigo
+JOIN [SIN_NOMBRE].[BI_TIEMPO] T ON T.Id = V.Tiempo
+GROUP BY T.Anio, T.Cuatrimestre, V.Recorrido, R.Origen, R.Destino
+GO
+
+
+--- Costo Promedio x Rango Etario
+CREATE OR ALTER VIEW [SIN_NOMBRE].[V_Promedio_Costo_Chofer]
+AS 
+SELECT 
+	 E.Descripcion
+	,AVG(Costo_Hora) AS [Promedio]
+FROM [SIN_NOMBRE].[BI_CHOFER] C
+JOIN [SIN_NOMBRE].BI_RANGO_EDAD E ON C.Rango_Edad = E.Id
+GROUP BY E.Descripcion
+GO
+
+--- GANANCIAS DE CAMION
+IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id(N'[SIN_NOMBRE].FN_COSTO_MANT') AND xtype IN (N'FN', N'IF', N'TF'))
+	DROP FUNCTION [SIN_NOMBRE].FN_COSTO_MANT
+GO
+
+CREATE OR ALTER FUNCTION 
+[SIN_NOMBRE].FN_COSTO_MANT(@patente NVARCHAR(15))
+RETURNS INT
+AS
+BEGIN
+ RETURN		(	SELECT
+					SUM(OT.Costo_Total) AS [Costo_Mantenimiento]
+				FROM [SIN_NOMBRE].BI_HECHO_ORDEN_TRABAJO OT
+				WHERE OT.Patente = @patente
+			)
+END
+GO
+
+CREATE OR ALTER VIEW [SIN_NOMBRE].[V_Camion_Ganancias] 
+AS
+SELECT 
+	V.Patente
+	,SUM(V.Total_Facturado - V.Costo_Total - [SIN_NOMBRE].FN_COSTO_MANT(V.Patente)) AS	[Ganancia]
+FROM [SIN_NOMBRE].BI_HECHO_VIAJE V
+GROUP BY V.Patente
 GO
